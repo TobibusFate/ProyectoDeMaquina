009aeda850ebb3420b8a57d821b2fdce5c07c821
Cels03694 <cellschool2020@gmail.com>
2022-09-25 21:29:20 -0300

    Agregada clase DB Init

---
+++ src/main/java/datos/DB_Init.java
@@ -0,0 +1,64 @@
+package datos;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB_Init {
+
+    private static Statement query = null;
+
+
+    public static void createTables() {
+        var conn = DatosBase.getInstance().getConnection();
+
+        try {
+            query = conn.createStatement();
+
+            query.execute("CREATE TABLE IF NOT EXISTS Productos ("
+                + "Prod_CODIGO INTEGER, "
+                + "Prod_NOMBRE VARCHAR(255) NOT NULL, "
+                + "Prod_CATEGORIA VARCHAR(255) NOT NULL, "
+                + "Prod_PRECIO REAL NOT NULL, "
+                + "Prod_STOCK INTEGER NOT NULL, "
+                + "Prod_STOCK_MINIMO INTEGER NOT NULL, "
+                + "PRIMARY KEY(Prod_CODIGO))"
+            );
+
+            // Recordar que pedidos tienen foreing key Proveedor
+            query.execute("CREATE TABLE IF NOT EXISTS Proveedores ("
+                // TO DO
+                + ")"
+            );
+            query.execute("CREATE TABLE IF NOT EXISTS Clientes ("
+                // TO DO
+                + ")"
+            );
+            // Pedidos tiene foreing key Producto (No recuerdo cómo hacer que sea más de 1)
+            query.execute("CREATE TABLE IF NOT EXISTS Pedidos ("
+                // TO DO
+                + ")"
+            );
+            // Ventas tiene foreing key Producto (No recuerdo cómo hacer que sea más de 1)
+            query.execute("CREATE TABLE IF NOT EXISTS Ventas ("
+                // TO DO
+                + ")"
+            );
+            // Otras?
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally { // finally se ejecuta siempre, no importa si hubo excepcion, así aseguramos que se cierra la conexión
+            DatosBase.getInstance().closeConnection();
+        }
+        
+            
+        
+    }
+
+    public static void initTables () {
+        // Llena las tablas creadas con valores basicos para usar y testear.
+    }
+
+
+}
